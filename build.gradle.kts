22d201e5d0c0e3ed3b8909e192c89c76b9f1944f
zhaoyuango <zhaoyuango@gmail.com>
2024-03-20 22:03:21 -0400

    updated project version number

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins { id("io.vacco.oss.gitflow") version "0.9.8" }
 
 group = "io.vacco.oruzka"
-version = "0.1.5.1"
+version = "0.2.0"
 
 configure<io.vacco.oss.gitflow.GsPluginProfileExtension> {
   addJ8Spec()
